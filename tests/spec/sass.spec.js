850f08b4719f07d356bcc36957173f9647bc619c
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-07 01:49:56 -0300

    Added .travis.yml and resolve the promise problem

--- tests/spec/sass.spec.js
+++ tests/spec/sass.spec.js
@@ -3,18 +3,19 @@ import { readFile } from '../helpers/utils'
 const { exec } = require('child_process')
 
 describe('sass task', () => {
-  before(done => {
-    exec('gulp sass', { cwd: __dirname })
-    exec('gulp sass --env production', { cwd: __dirname }, () => { done() })
+  it('the `gulp sass` command should be generate a non minified file', done => {
+    exec('gulp sass', { cwd: __dirname }, () => {
+      const expected = readFile('expected/expected.css')
+      expect(expected).to.equal(readFile('output/main.css'))
+      done()
+    })
   })
 
-  it('the `gulp sass` command should be generate a non minified file', () => {
-    const expected = readFile('expected/expected.css')
-    expect(expected).to.equal(readFile('output/main.css'))
-  })
-
-  it('the `gulp sass --env production` command should be generate a minified file', () => {
-    const expected = readFile('expected/expected.min.css')
-    expect(expected).to.equal(readFile('output/main.min.css'))
+  it('the `gulp sass --env production` command should be generate a minified file', done => {
+    exec('gulp sass --env production', { cwd: __dirname }, () => {
+      const expected = readFile('expected/expected.min.css')
+      expect(expected).to.equal(readFile('output/main.min.css'))
+      done()
+    })
   })
 })
--- tests/spec/stylesheets.spec.js
+++ tests/spec/stylesheets.spec.js
@@ -3,18 +3,19 @@ import { readFile } from '../helpers/utils'
 const { exec } = require('child_process')
 
 describe('css task', () => {
-  before(done => {
-    exec('gulp css', { cwd: __dirname })
-    exec('gulp css --env production', { cwd: __dirname }, () => { done() })
+  it('the `gulp css` command should be generate a non minified file', done => {
+    exec('gulp css', { cwd: __dirname }, () => {
+      const expected = readFile('expected/expected.css')
+      expect(expected).to.equal(readFile('output/main.css'))
+      done()
+    })
   })
 
-  it('the `gulp css` command should be generate a non minified file', () => {
-    const expected = readFile('expected/expected.css')
-    expect(expected).to.equal(readFile('output/main.css'))
-  })
-
-  it('the `gulp css --env production` command should be generate a minified file', () => {
-    const expected = readFile('expected/expected.min.css')
-    expect(expected).to.equal(readFile('output/main.min.css'))
+  it('the `gulp css --env production` command should be generate a minified file', done => {
+    exec('gulp css --env production', { cwd: __dirname }, () => {
+      const expected = readFile('expected/expected.min.css')
+      expect(expected).to.equal(readFile('output/main.min.css'))
+      done()
+    })
   })
 })
