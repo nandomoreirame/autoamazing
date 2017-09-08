4bec589d35916c07a14af1cb2bce36ec9086ddbc
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-08 17:13:37 -0300

    added node-bourbon and sass settings

--- src/stylesheets.js
+++ src/stylesheets.js
@@ -1,6 +1,7 @@
 import plugins from 'gulp-load-plugins'
 import { merge } from 'lodash'
 import { config } from './helpers/config'
+import bourbon from 'node-bourbon'
 
 const $ = plugins()
 
@@ -9,16 +10,35 @@ const defaults = {
   src: ['./src/main.sass'],
   dest: './dist',
   env: 'production',
-  sass: true
+  sass: true,
+  sassSettings: {
+    sourcemap: true,
+    noCache: true,
+    style: 'expanded',
+    sourceComments: true,
+    includePaths: [
+      bourbon.includePaths,
+      `node_modules`,
+      `src/assets/sass`,
+      `assets/sass`,
+      `sass`
+    ]
+  }
 }
 
 module.exports = (gulp, options, othersTasks = []) => {
+  let sassSettings = {}
+
   const opts = merge({}, defaults, options)
   const isProduction = opts.env === 'production'
 
+  if (opts.sass) {
+    sassSettings = merge({}, opts.sassSettings)
+  }
+
   gulp.task(opts.taskname, done => gulp.src(opts.src)
     .pipe($.plumber(config.plumber))
-    .pipe(opts.sass ? $.sass()
+    .pipe(opts.sass ? $.sass(sassSettings)
       .on('error', $.sass.logError) : $.util.noop())
     .pipe($.combineMq())
     .pipe(isProduction ? $.cssnano(config.cssnano) : $.util.noop())
