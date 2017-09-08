581a2f83aaf5394e81d84fc250f936bd12135de5
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-08 18:57:29 -0300

    Removed describe.only from tests

--- tests/spec/copy.spec.js
+++ tests/spec/copy.spec.js
@@ -2,7 +2,7 @@
 // import { readFile } from '../helpers/utils'
 const { exec } = require('child_process')
 
-describe.only('copy task', () => {
+describe('copy task', () => {
   describe('gulp copy', function () {
     it('the `gulp copy` command should', done => {
       exec('gulp copy', { cwd: __dirname }, () => {
--- tests/spec/eslint.spec.js
+++ tests/spec/eslint.spec.js
@@ -2,7 +2,7 @@
 // import { readFile } from '../helpers/utils'
 const { exec } = require('child_process')
 
-describe.only('eslint task', () => {
+describe('eslint task', () => {
   describe('gulp eslint', function () {
     it('the `gulp eslint` command should', done => {
       exec('gulp eslint', { cwd: __dirname }, () => {
